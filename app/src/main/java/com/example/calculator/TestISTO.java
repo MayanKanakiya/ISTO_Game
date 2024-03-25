a3a511075226e22b36911c52af954ee6a4347b81
Mayank <Kanakiyamayank182003@gmail.com>
2024-03-25 22:46:31 +0530

    25-03-2024
    40% complete, move into inside only one box when shuffle the 4 random image

--- app/src/main/java/com/example/calculator/TestISTO.java
+++ app/src/main/java/com/example/calculator/TestISTO.java
@@ -1,6 +1,8 @@
 package com.example.calculator;
 
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -18,9 +20,14 @@ public class TestISTO extends AppCompatActivity {
     private Button A2Btn;
     private Button A3Btn;
     private Button A4Btn;
-    private List<Integer> allImageRes;
-    private List<Integer> selectedImageRes;
-    private List<ImageView> lstImg;
+//    This below three lines of code for shuffling random 8 images(Front-4 and back-4) into 4 imageView
+    private List<Integer> allImageResRandom;
+    private List<Integer> selectedImageResRandom;
+    private List<ImageView> lstImgRandom;
+
+//  This below three lines of code for get imageView id if image are place into them : image 4 and imageView 100
+    private List<Integer> allImageResDashBoard;
+    private List<ImageView> lstImgDashboard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,69 +35,206 @@ public class TestISTO extends AppCompatActivity {
         setContentView(R.layout.activity_test_isto);
         startBtn = findViewById(R.id.startBtn);
 
-        allImageRes = new ArrayList<>();
-        allImageRes.add(R.drawable.back1);
-        allImageRes.add(R.drawable.back2);
-        allImageRes.add(R.drawable.back3);
-        allImageRes.add(R.drawable.back4);
-        allImageRes.add(R.drawable.front1);
-        allImageRes.add(R.drawable.front2);
-        allImageRes.add(R.drawable.front3);
-        allImageRes.add(R.drawable.front4);
+//        Random image shuffling
+        allImageResRandom = new ArrayList<>();
+        allImageResRandom.add(R.drawable.back1);
+        allImageResRandom.add(R.drawable.back2);
+        allImageResRandom.add(R.drawable.back3);
+        allImageResRandom.add(R.drawable.back4);
+        allImageResRandom.add(R.drawable.front1);
+        allImageResRandom.add(R.drawable.front2);
+        allImageResRandom.add(R.drawable.front3);
+        allImageResRandom.add(R.drawable.front4);
 
-        selectedImageRes = new ArrayList<>();
-        lstImg = new ArrayList<>();
-        lstImg.add(findViewById(R.id.imageView1));
-        lstImg.add(findViewById(R.id.imageView2));
-        lstImg.add(findViewById(R.id.imageView3));
-        lstImg.add(findViewById(R.id.imageView4));
+        selectedImageResRandom = new ArrayList<>();
+        lstImgRandom = new ArrayList<>();
+        lstImgRandom.add(findViewById(R.id.imageView1));
+        lstImgRandom.add(findViewById(R.id.imageView2));
+        lstImgRandom.add(findViewById(R.id.imageView3));
+        lstImgRandom.add(findViewById(R.id.imageView4));
+
+
+//        Dashboard
+        allImageResDashBoard = new ArrayList<>();
+        allImageResDashBoard.add(R.drawable.p1);
+        allImageResDashBoard.add(R.drawable.p2);
+        allImageResDashBoard.add(R.drawable.p3);
+        allImageResDashBoard.add(R.drawable.p4);
+
+        lstImgDashboard = new ArrayList<>();
+        lstImgDashboard.add(findViewById(R.id.A1_P1_0));
+        lstImgDashboard.add(findViewById(R.id.A2_P2_0));
+        lstImgDashboard.add(findViewById(R.id.A3_P3_0));
+        lstImgDashboard.add(findViewById(R.id.A4_P4_0));
+        lstImgDashboard.add(findViewById(R.id.A1_P1_1));
+        lstImgDashboard.add(findViewById(R.id.A2_P2_1));
+        lstImgDashboard.add(findViewById(R.id.A3_P3_1));
+        lstImgDashboard.add(findViewById(R.id.A4_P4_1));
+        lstImgDashboard.add(findViewById(R.id.A1_P1_2));
+        lstImgDashboard.add(findViewById(R.id.A2_P2_2));
+        lstImgDashboard.add(findViewById(R.id.A3_P3_2));
+        lstImgDashboard.add(findViewById(R.id.A4_P4_2));
+        lstImgDashboard.add(findViewById(R.id.A1_P1_3));
+        lstImgDashboard.add(findViewById(R.id.A2_P2_3));
+        lstImgDashboard.add(findViewById(R.id.A3_P3_3));
+        lstImgDashboard.add(findViewById(R.id.A4_P4_3));
+        lstImgDashboard.add(findViewById(R.id.A1_P1_4));
+        lstImgDashboard.add(findViewById(R.id.A2_P2_4));
+        lstImgDashboard.add(findViewById(R.id.A3_P3_4));
+        lstImgDashboard.add(findViewById(R.id.A4_P4_4));
+        lstImgDashboard.add(findViewById(R.id.A1_P1_5));
+        lstImgDashboard.add(findViewById(R.id.A2_P2_5));
+        lstImgDashboard.add(findViewById(R.id.A3_P3_5));
+        lstImgDashboard.add(findViewById(R.id.A4_P4_5));
+        lstImgDashboard.add(findViewById(R.id.A1_P1_6));
+        lstImgDashboard.add(findViewById(R.id.A2_P2_6));
+        lstImgDashboard.add(findViewById(R.id.A3_P3_6));
+        lstImgDashboard.add(findViewById(R.id.A4_P4_6));
+        lstImgDashboard.add(findViewById(R.id.A1_P1_7));
+        lstImgDashboard.add(findViewById(R.id.A2_P2_7));
+        lstImgDashboard.add(findViewById(R.id.A3_P3_7));
+        lstImgDashboard.add(findViewById(R.id.A4_P4_7));
+        lstImgDashboard.add(findViewById(R.id.A1_P1_8));
+        lstImgDashboard.add(findViewById(R.id.A2_P2_8));
+        lstImgDashboard.add(findViewById(R.id.A3_P3_8));
+        lstImgDashboard.add(findViewById(R.id.A4_P4_8));
+        lstImgDashboard.add(findViewById(R.id.A1_P1_9));
+        lstImgDashboard.add(findViewById(R.id.A2_P2_9));
+        lstImgDashboard.add(findViewById(R.id.A3_P3_9));
+        lstImgDashboard.add(findViewById(R.id.A4_P4_9));
+        lstImgDashboard.add(findViewById(R.id.A1_P1_10));
+        lstImgDashboard.add(findViewById(R.id.A2_P2_10));
+        lstImgDashboard.add(findViewById(R.id.A3_P3_10));
+        lstImgDashboard.add(findViewById(R.id.A4_P4_10));
+        lstImgDashboard.add(findViewById(R.id.A1_P1_11));
+        lstImgDashboard.add(findViewById(R.id.A2_P2_11));
+        lstImgDashboard.add(findViewById(R.id.A3_P3_11));
+        lstImgDashboard.add(findViewById(R.id.A4_P4_11));
+        lstImgDashboard.add(findViewById(R.id.A1_P1_12));
+        lstImgDashboard.add(findViewById(R.id.A2_P2_12));
+        lstImgDashboard.add(findViewById(R.id.A3_P3_12));
+        lstImgDashboard.add(findViewById(R.id.A4_P4_12));
+        lstImgDashboard.add(findViewById(R.id.A1_P1_13));
+        lstImgDashboard.add(findViewById(R.id.A2_P2_13));
+        lstImgDashboard.add(findViewById(R.id.A3_P3_13));
+        lstImgDashboard.add(findViewById(R.id.A4_P4_13));
+        lstImgDashboard.add(findViewById(R.id.A1_P1_14));
+        lstImgDashboard.add(findViewById(R.id.A2_P2_14));
+        lstImgDashboard.add(findViewById(R.id.A3_P3_14));
+        lstImgDashboard.add(findViewById(R.id.A4_P4_14));
+        lstImgDashboard.add(findViewById(R.id.A1_P1_15));
+        lstImgDashboard.add(findViewById(R.id.A2_P2_15));
+        lstImgDashboard.add(findViewById(R.id.A3_P3_15));
+        lstImgDashboard.add(findViewById(R.id.A4_P4_15));
+        lstImgDashboard.add(findViewById(R.id.A1_P1_16));
+        lstImgDashboard.add(findViewById(R.id.A2_P2_16));
+        lstImgDashboard.add(findViewById(R.id.A3_P3_16));
+        lstImgDashboard.add(findViewById(R.id.A4_P4_16));
+        lstImgDashboard.add(findViewById(R.id.A1_P1_17));
+        lstImgDashboard.add(findViewById(R.id.A2_P2_17));
+        lstImgDashboard.add(findViewById(R.id.A3_P3_17));
+        lstImgDashboard.add(findViewById(R.id.A4_P4_17));
+        lstImgDashboard.add(findViewById(R.id.A1_P1_18));
+        lstImgDashboard.add(findViewById(R.id.A2_P2_18));
+        lstImgDashboard.add(findViewById(R.id.A3_P3_18));
+        lstImgDashboard.add(findViewById(R.id.A4_P4_18));
+        lstImgDashboard.add(findViewById(R.id.A1_P1_19));
+        lstImgDashboard.add(findViewById(R.id.A2_P2_19));
+        lstImgDashboard.add(findViewById(R.id.A3_P3_19));
+        lstImgDashboard.add(findViewById(R.id.A4_P4_19));
+        lstImgDashboard.add(findViewById(R.id.A1_P1_20));
+        lstImgDashboard.add(findViewById(R.id.A2_P2_20));
+        lstImgDashboard.add(findViewById(R.id.A3_P3_20));
+        lstImgDashboard.add(findViewById(R.id.A4_P4_20));
+        lstImgDashboard.add(findViewById(R.id.A1_P1_21));
+        lstImgDashboard.add(findViewById(R.id.A2_P2_21));
+        lstImgDashboard.add(findViewById(R.id.A3_P3_21));
+        lstImgDashboard.add(findViewById(R.id.A4_P4_21));
+        lstImgDashboard.add(findViewById(R.id.A1_P1_22));
+        lstImgDashboard.add(findViewById(R.id.A2_P2_22));
+        lstImgDashboard.add(findViewById(R.id.A3_P3_22));
+        lstImgDashboard.add(findViewById(R.id.A4_P4_22));
+        lstImgDashboard.add(findViewById(R.id.A1_P1_23));
+        lstImgDashboard.add(findViewById(R.id.A2_P2_23));
+        lstImgDashboard.add(findViewById(R.id.A3_P3_23));
+        lstImgDashboard.add(findViewById(R.id.A4_P4_23));
+        lstImgDashboard.add(findViewById(R.id.A1_P1_24));
+        lstImgDashboard.add(findViewById(R.id.A2_P2_24));
+        lstImgDashboard.add(findViewById(R.id.A3_P3_24));
+        lstImgDashboard.add(findViewById(R.id.A4_P4_24));
 
         startBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 onRandomizeButtonClick();
                 updateImageViews();
-                showImageNameInToast();
+                showImageNameInRandom();
             }
         });
-//        This is the code to choose which player can take forword - start here
-        
-//        This is the code to choose which player can take forword - end here
     }
 
     // This code randomly selects 4 images out of 8
     public void onRandomizeButtonClick() {
-        Collections.shuffle(allImageRes);
-        selectedImageRes.clear();
-        selectedImageRes.addAll(allImageRes.subList(0, 4));
+        Collections.shuffle(allImageResRandom);
+        selectedImageResRandom.clear();
+        selectedImageResRandom.addAll(allImageResRandom.subList(0, 4));
     }
 
     // This code updates the ImageView elements with the selected images
     private void updateImageViews() {
-        for (int i = 0; i < selectedImageRes.size(); i++) {
-            lstImg.get(i).setImageResource(selectedImageRes.get(i));
+        for (int i = 0; i < selectedImageResRandom.size(); i++) {
+            lstImgRandom.get(i).setImageResource(selectedImageResRandom.get(i));
         }
     }
 
     // This code retrieves the name of the currently displayed image and shows it in a Toast
-    private void showImageNameInToast() {
+    private void showImageNameInRandom() {
         StringBuilder toastMessage = new StringBuilder();
-        for (int i = 0; i < lstImg.size(); i++) {
-            ImageView imageView = lstImg.get(i);
-            int resourceId = selectedImageRes.get(i);
-            String resourceName = getResourceName(resourceId);
-            toastMessage.append("Image of ImageView ").append(i + 1).append(": ").append(resourceName).append("\n");
-        }
+
+        int frontCount = 0;
+        int backCount = 0;
+
+        // Count the number of front and back images
+        for (Integer imageRes : selectedImageResRandom) {
+            if (isFront(imageRes)) {
+                frontCount++;
+            } else {
+                backCount++;
+            }}
+
+        // Update images in lstImgDashboard based on front and back count
+        if (frontCount > 0) {
+            for (int i = 0; i < lstImgDashboard.size() && i < frontCount; i++) {
+                ImageView imageView = lstImgDashboard.get(i);
+                int newImageIndex = i + frontCount;
+                if (newImageIndex < allImageResDashBoard.size()) {
+                    imageView.setImageResource(allImageResDashBoard.get(newImageIndex));
+                }}
+        } else if (backCount == 4) {
+            for (int i = 0; i < lstImgDashboard.size(); i++) {
+                ImageView imageView = lstImgDashboard.get(i);
+                int newImageIndex = i + 8; // Move 8 steps ahead
+                if (newImageIndex < allImageResDashBoard.size()) {
+                    imageView.setImageResource(allImageResDashBoard.get(newImageIndex));
+                }}}
+
+        // Show the toast message
         showToast(toastMessage.toString());
     }
 
-    private String getResourceName(int resourceId) {
-        return getResources().getResourceEntryName(resourceId);
+    // Check if the image is a front image
+    private boolean isFront(int imageRes) {
+        return imageRes == R.drawable.front1 || imageRes == R.drawable.front2 ||
+                imageRes == R.drawable.front3 || imageRes == R.drawable.front4;
     }
-
 
     // Helper method to display a Toast message
     private void showToast(String message) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
+    // This method for retrieve the image resource name in randomly move
+    private String getResourceName(int resourceId) {
+        return getResources().getResourceEntryName(resourceId);
     }
 }
